bf0c9783de2df285f2842dc429391a71dd8e09e6
Jordan Williams <jordan@jwillikers.com>
2021-08-04 14:57:39 -0500

    Switch to new development workflow
    Target Fedora 34.
    Remove asdf and direnv instructions.
    Use Pylint and Black.
    Add GitHub CI workflow.
    Add support for pre-commit.
    Update and simplify the README.

--- chunkify.py
+++ chunkify.py
@@ -7,16 +7,16 @@ from typing import List
 # chunkify(‘ABCDEFG’, 3, ‘x’) -> [‘ABC’, ‘DEF’, Gxx’]
 # Adapted from the grouper recipe found in the itertools documentation:
 # https://docs.python.org/3/library/itertools.html#itertools-recipes
-def chunkify(s: str, chunk_size: int, fill: str = 'x') -> List[str]:
+def chunkify(s: str, chunk_size: int, fill: str = "x") -> List[str]:
 
     if len(fill) != 1:
-        raise ValueError('The fill string must consist of exactly one character')
+        raise ValueError("The fill string must consist of exactly one character")
 
     if chunk_size < 1:
-        raise ValueError('The chunk size must be positive')
+        raise ValueError("The chunk size must be positive")
 
     if len(s) < chunk_size:
         return [s.ljust(chunk_size, fill)]
 
     args = [iter(s)] * chunk_size
-    return [''.join(chars) for chars in list(zip_longest(*args, fillvalue=fill))]
+    return ["".join(chars) for chars in list(zip_longest(*args, fillvalue=fill))]
--- test_chunkify.py
+++ test_chunkify.py
@@ -3,66 +3,70 @@ from chunkify import chunkify
 
 
 def test_accept_empty_string():
-    assert chunkify('', 5, 'x') == ['xxxxx']
+    assert chunkify("", 5, "x") == ["xxxxx"]
 
 
 def test_should_reject_chunk_size_of_zero():
     with pytest.raises(ValueError):
-        chunkify('ABCDEFG', 0, 'x')
+        chunkify("ABCDEFG", 0, "x")
 
 
 def test_should_reject_negative_chunk_size():
     with pytest.raises(ValueError):
-        chunkify('ABCDEFG', -1, 'x')
+        chunkify("ABCDEFG", -1, "x")
 
 
 def test_should_reject_s_given_an_int():
     with pytest.raises(TypeError):
-        chunkify(1, 1, 'x')
+        chunkify(1, 1, "x")
 
 
 def test_should_reject_s_given_an_float():
     with pytest.raises(TypeError):
-        chunkify(5.5, 1, 'x')
+        chunkify(5.5, 1, "x")
 
 
 def test_should_reject_s_given_a_list_of_ints():
     with pytest.raises(TypeError):
-        chunkify([1, 2, 3, 4, 5], 1, 'x')
+        chunkify([1, 2, 3, 4, 5], 1, "x")
 
 
 def test_should_reject_given_an_int_for_fill():
     with pytest.raises(TypeError):
-        chunkify('ABCDEFG', 3, 1)
+        chunkify("ABCDEFG", 3, 1)
 
 
 def test_should_reject_given_a_float_for_fill():
     with pytest.raises(TypeError):
-        chunkify('ABCDEFG', 3, 1.2)
+        chunkify("ABCDEFG", 3, 1.2)
 
 
 def test_should_reject_given_a_list_for_fill():
     with pytest.raises(ValueError):
-        chunkify('ABCDEFG', 3, [])
+        chunkify("ABCDEFG", 3, [])
 
 
 def test_should_reject_given_a_string_with_more_than_a_single_character_for_fill():
     with pytest.raises(ValueError):
-        chunkify('ABCDEFG', 3, 'xx')
+        chunkify("ABCDEFG", 3, "xx")
 
 
 def test_should_reject_a_list_of_strings_given_for_fill():
     with pytest.raises(TypeError):
-        chunkify('ABCDEFG', 3, ['x'])
+        chunkify("ABCDEFG", 3, ["x"])
 
 
 def test_chunks_of_size_one():
-    assert chunkify('ABCDEFG', 1, 'z') == ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+    assert chunkify("ABCDEFG", 1, "z") == ["A", "B", "C", "D", "E", "F", "G"]
 
 
 def test_should_chunkify_the_example_correctly():
-    assert chunkify('ABCDEFG', 3, 'x') == ['ABC', 'DEF', 'Gxx']
+    assert chunkify("ABCDEFG", 3, "x") == ["ABC", "DEF", "Gxx"]
 
 
 def test_should_work_just_the_same_given_a_list_of_strings():
-    assert chunkify(['A', 'B', 'C', 'D', 'E', 'F', 'G'], 3, 'x') == ['ABC', 'DEF', 'Gxx']
+    assert chunkify(["A", "B", "C", "D", "E", "F", "G"], 3, "x") == [
+        "ABC",
+        "DEF",
+        "Gxx",
+    ]
